3a37d97f2ff394cb8fff76c36f51f23896f8517d
krzykaczu <krzywiec@gmail.com>
2019-07-06 12:38:38 +0200

    Add Heroku's endpoint config

--- src/js/utils/endpoint.js
+++ src/js/utils/endpoint.js
@@ -1,3 +1,4 @@
-const endpoint = "http://localhost:3000/data";
+// const endpoint = "http://localhost:4000/data";
+const endpoint = "https://real-estate-3m-server.herokuapp.com/data"
 
 export default endpoint
