d901802a8d3054573084b2f4ec9a47db80949d4a
Kacper Krzywiec <krzywiec@gmail.com>
2020-06-28 12:01:02 +0200

    Fix HMR issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,38 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: ['./src/js/main.ts'],
+  // output: {
+  //   path: path.resolve(__dirname, 'dist'),
+  //   filename: 'main.js'
+  // },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      inject: false,
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
   devtool: 'eval-source-map',
   resolve: {
     modules: [__dirname, 'node_modules'],
-    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    extensions: ['*', '.js', '.ts'],
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 5000,
+    contentBase: path.resolve(__dirname, 'src'),
+    watchContentBase: true,
+    hot: true,
+    open: true,
+    inline: true
   },
   module: {
     rules: [
@@ -25,9 +47,4 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html'
-    })
-  ]
 };
