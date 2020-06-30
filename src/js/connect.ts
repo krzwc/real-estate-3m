97e4cff6d8be36496ceafe5b338e32fdf765867d
Kacper Krzywiec <krzywiec@gmail.com>
2020-06-30 17:55:18 +0200

    Bump npm packages, wip - keylisteners

--- src/js/connect.ts
+++ src/js/connect.ts
@@ -1,25 +1,5 @@
-/*import axios from 'axios';*/
-
 import { getAds } from './api';
 import { toAdData } from './model';
-
-/*const connect = async () => {
-    let locations = [];
-    try {
-        await axios
-            .get(endpoint)
-            .then((response) => {
-                locations.push(...response.data);
-                return locations;
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    } catch (error) {
-        console.error(error);
-    }
-    return locations;
-};*/
 
 const connect = (endpoint) => getAds(endpoint).map((json) => json.map(toAdData));
 
--- src/js/main.ts
+++ src/js/main.ts
@@ -13,32 +13,14 @@ import { matchAndDisplayLocations } from './utils/panelUtils';
 let map; // google map api data
 let markerArr = []; // array of all markers
 let matchedMarkerArr = []; // array of all markers based on matched locations
-let locations; //REST API data
 
 //resolve all locations and then aplly them to the map
 let mapsCallback = () => {
-    /*let promise = new Promise(function (resolve, reject) {
-        resolve(
-            connect()
-        .then(data => {
-          locations = [...data];
-          return locations
-        })
-            connect.fork(
-                console.error,
-                compose(console.log, (locArr) => {
-                    locations = [...locArr];
-                }),
-            ),
-        );
-    });
-    promise.then((locations) => ([map, markerArr, matchedMarkerArr] = myMap(locations, suggestions)));
-    */
     connect(endpoint).fork(
         console.error,
         compose(
-            console.log,
             (locations) => ([map, markerArr, matchedMarkerArr] = myMap(locations, suggestions)),
+            addEventListeners
         ),
     );
 };
@@ -47,11 +29,17 @@ window.mapsCallback = mapsCallback;
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
 
-searchInput.addEventListener('keyup', (e) =>
-    matchAndDisplayLocations(locations, e, matchedMarkerArr, map, suggestions),
-);
-searchInput.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-        e.preventDefault();
-    }
-});
+const addEventListeners = (locations) => {
+    searchInput.addEventListener('keyup', (e) =>
+        matchAndDisplayLocations(locations, e, matchedMarkerArr, map, suggestions),
+    );
+    searchInput.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+        }
+    });
+
+    return locations
+};
+
+
