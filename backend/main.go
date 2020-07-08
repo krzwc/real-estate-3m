8ba69e7f9646fa62d03b06673e9cb29863782e1c
Kacper Krzywiec <krzywiec@gmail.com>
2020-07-08 06:12:03 +0200

    WIP add geocoding in golang backend

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"net/url"
+	"strconv"
 )
 
 type AdData struct {
@@ -23,11 +25,47 @@ type AdDataNodes struct {
 	Data []AdData `json:"data"`
 }
 
+type SingleAttr struct {
+	ObjectID   string
+	SingleLine string
+}
+
+type Attributes struct {
+	Attributes SingleAttr
+}
+
+type Records struct {
+	Records []Attributes
+}
+
 func get(w http.ResponseWriter, r *http.Request) {
 	filename := "./assets/scrap3m.json"
 	file, _ := ioutil.ReadFile(filename)
 	data := AdDataNodes{}
 	_ = json.Unmarshal([]byte(file), &data)
+
+	token := "EfpuXlCvYMNSZ1Vbn5EZB9jfbNbqvvDqToqfqnq7w6Xlys3gKZV8aE8ixOU2S1NEFsA7aXqZEJO8uaGD66s6RsWeY6WX75WJaobU5WUOfgpc-qFDo-7ntHiX7b03UejUnAiw8ZDjLUNn9H2kcZtSZ73bwVAxtIv1VQc52Nz3CDEMHuJiFGV54pt8aElrxzGXfqw3LAKG0RpAQCBIRBmZ96G5kYr52VA5Dv55sqEcu98."
+	var records Records
+	for i, v := range data.Data {
+		singleAttr := SingleAttr{
+			ObjectID: strconv.Itoa(i + 1),
+			SingleLine: v.Loc,
+		}
+		attr := Attributes{singleAttr}
+		records.Records = append(records.Records, attr)
+	}
+	marshaledRecords, err := json.Marshal(records)
+	if err != nil {
+			fmt.Println(err)
+			return
+		}
+ 	encodedRecords := url.QueryEscape(string(marshaledRecords))
+	URL := "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/geocodeAddresses?addresses=" + encodedRecords + "&f=pjson&token=" + token
+
+	fmt.Println(URL)
+
+	//resp, err := http.Get("http://example.com/")
+
 	b, err := json.Marshal(data.Data)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
